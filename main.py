4287a6733d2e4277fc01fb31623d8f7fd4fd3d2a
ling612 <147597317+ling612@users.noreply.github.com>
2023-10-20 15:17:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         menu()
         choice=int(input('请输入所要选择的序号：'))
-        if choice in [0,1,2,3,4,5,6,7]
+        if choice in [0,1,2,3,4,5,6,7]:
             if choice == 0:
                 anwser=input('是否确认退出系统？y/n')
                 if anwser == 'y':
